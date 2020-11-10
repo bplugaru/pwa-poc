f6f186ab077cfad49eed3e99939ccb585e55aa57
Bogdan Plugaru <bplugaru@gmail.com>
2020-11-10 10:55:18 +0200

    add refresh when new app is available

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 const withPWA = require('next-pwa')
- 
+const runtimeCaching = require('next-pwa/cache')
+runtimeCaching[0].handler = 'StaleWhileRevalidate'
+
 module.exports = withPWA({
     pwa: {
-        dest: 'public'
+      dest: 'public',
+      register: false,
+      skipWaiting: false,
+      runtimeCaching
     }
 });
