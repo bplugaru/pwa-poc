c08bd126c19e68257090256b045a43e0cc14fca9
Bogdan Plugaru <bplugaru@gmail.com>
2020-11-10 11:01:57 +0200

    modify react

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ export default function Home() {
       wb.register()
     }
   }, [])
-  
+
   return (
     <div className={styles.container}>
       <Head>
@@ -84,7 +84,7 @@ export default function Home() {
     <Nav></Nav>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Learn <a href="https://nextjs.org">Next.js!</a>
+          Learn <a href="https://nextjs.org">reactjs!</a>
         </h1>
 
         <p className={styles.description}>
