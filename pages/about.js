d62944ff6977acac9c7f46d91099840120e180f8
Bogdan Plugaru <bplugaru@gmail.com>
2020-11-10 16:40:59 +0200

    change url

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,7 @@ import styles from "../styles/Home.module.css";
 export default function About() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>About</h1>
+      <h1 className={styles.title}>About.</h1>
     </div>
   );
 }
--- pages/contact.js
+++ pages/contact.js
@@ -4,7 +4,7 @@ import styles from "../styles/Home.module.css";
 export default function About() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Contact</h1>
+      <h1 className={styles.title}>Contact.</h1>
     </div>
   );
 }
