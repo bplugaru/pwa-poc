02e37f012e460af3d75c8b9b52f3b44df342f2ca
Bogdan Plugaru <bplugaru@gmail.com>
2020-11-10 10:49:40 +0200

    add pwa support

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Nav from '../components/nav'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -8,10 +9,10 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+    <Nav></Nav>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Learn <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
