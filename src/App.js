783123f079f56b3c8c8ba53c3d07b4147b9edc66
Bogdan Plugaru <bplugaru@gmail.com>
2020-11-11 10:40:33 +0200

    add pwa

--- src/App.js
+++ src/App.js
@@ -1,15 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useSnackbar } from "notistack";
+import { Button } from "@material-ui/core";
+import * as serviceWorker from "./serviceWorkerRegistration";
 
 function App() {
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+  const [state, setState] = useState({
+    newVersionAvailable: false,
+    waitingWorker: {},
+  });
+
+  const onServiceWorkerUpdate = (registration) => {
+    console.log("update");
+    setState({
+      newVersionAvailable: true,
+      waitingWorker: registration && registration.waiting,
+    });
+  };
+  const updateServiceWorker = () => {
+    const { waitingWorker } = state;
+    waitingWorker && waitingWorker.postMessage({ type: "SKIP_WAITING" });
+    this.setState({ newVersionAvailable: false });
+    window.location.reload(true);
+  };
+  const refreshAction = () => (
+    <Button
+      className="snackbar-button"
+      size="small"
+      onClick={updateServiceWorker}
+    >
+      {"refresh"}
+    </Button>
+  );
+
+  useEffect(() => {
+    enqueueSnackbar("A new version was released", {
+      persist: true,
+      variant: "success",
+      action: refreshAction(),
+    });
+  }, []);
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === "production") {
+      serviceWorker.register({ onUpdate: onServiceWorkerUpdate });
+    }
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
+import { SnackbarProvider } from "notistack";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SnackbarProvider maxSnack={3}>
+      <App />
+    </SnackbarProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
