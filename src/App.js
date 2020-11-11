bd780a20f3521dc57d3980a4ffb7304db051494f
Bogdan Plugaru <bplugaru@gmail.com>
2020-11-11 11:00:05 +0200

    add demo

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React?
+          Learn React? demo
         </a>
       </header>
     </div>
